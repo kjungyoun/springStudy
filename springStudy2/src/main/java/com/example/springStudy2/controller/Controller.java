affefb27b703b107e361660eecebe97000d4748a
kjungyoun <rlawjddbs963@naver.com>
2020-08-06 01:47:50 +0900

    2020/08/06 db연동스터디

--- springStudy2/src/main/java/com/example/springStudy2/controller/Controller.java
+++ springStudy2/src/main/java/com/example/springStudy2/controller/Controller.java
@@ -6,6 +6,7 @@ import com.example.springStudy2.service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @org.springframework.stereotype.Controller //controller 객체임을 명시해서 얘를 이용해 controller에 bean객체를 등록한다
@@ -14,26 +15,46 @@ public class Controller {
     private final Service service;
 
     @Autowired
+
     public Controller(Service service) { //controller 생성자, 이 생성자는 bean객체만을 인수로 받음 따라서 service를 bean 객체로 등록해야함
         this.service = service;
     }
 
     @GetMapping("/join")
-    public String join(@RequestParam(value="name",required = true)String name,
-                       Model model){
+    public String join(@RequestParam(value = "name", required = true) String name,
+                       Model model) {
         Member member = new Member();
         member.setName(name);
         service.join(member);
-        model.addAttribute("name",name);
-    return "join";
+        model.addAttribute("name", name);
+        return "join";
     }
 
     @GetMapping("/find")
-    public String find(@RequestParam(value = "name",required = true)String name,
-                       Model model){
+    public String find(@RequestParam(value = "name", required = true) String name,
+                       Model model) {
         service.findMemberByname(name);
-        model.addAttribute("name",service.findMemberByname(name).get().getName());
+        model.addAttribute("name", service.findMemberByname(name).get().getName());
         // findMemberByname(name).get은 member 객체를 뽑는것이고 거기에 .getName()을 하면 member변수의 name을 뽑는 것이다.
         return "find";
+    }
+
+    @GetMapping("/member/new")
+    public String newForm(){
+        return "member/createMemberForm"; //new에 접근하면 createMemberForm이라는 페이지를 리턴
+    }
+
+    @PostMapping("/member/new")
+    public String join(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+        service.join(member);
+
+        return "redirect:/"; // 루트페이지로 다시 이동시켜줌
+    }
+    @GetMapping("/list")
+    public String list(Model model){
+        model.addAttribute("members",service.findMembers());
+        return "member/memberList";
     }
 }
