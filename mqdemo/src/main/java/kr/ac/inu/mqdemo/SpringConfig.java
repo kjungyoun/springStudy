7e58de52f70b5990b785e9a77c5378d97ba158ec
kjungyoun <rlawjddbs963@naver.com>
2020-08-18 10:32:14 +0900

    2020/08/18 RabbitMQ update

--- mqdemo/src/main/java/kr/ac/inu/mqdemo/SpringConfig.java
+++ mqdemo/src/main/java/kr/ac/inu/mqdemo/SpringConfig.java
@@ -10,8 +10,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
-    static final String QUEUE_NAME = "inu";
-    static final String EXCHANGE_NAME ="inu";
+    static final String QUEUE_NAME = "Kim"; // 임의로 정해도 된다
+    static final String EXCHANGE_NAME ="Kim"; // 임의로 정해도 된다
     static final String ROUNTING_KEY = "foo.bar.#";
     @Bean
     Queue queue(){
@@ -35,17 +35,18 @@ public class SpringConfig {
         SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
 
         container.setConnectionFactory(factory);  //서버 ip, port, 이름, 패스워드
-        container.setQueueNames(QUEUE_NAME);     //처리할 큐들 목록 관리(구독)
-        container.setMessageListener(adapter);  // 어떻게 처리할지
+//        container.setQueueNames(QUEUE_NAME);     //처리할 큐들 목록 관리(구독)
+//        container.setMessageListener(adapter);  // 어떻게 처리할지
+        container.setAcknowledgeMode(AcknowledgeMode.MANUAL); // 이렇게 하면 메세지를 다 읽어도 자동으로 사라지지 않음, 따라서 수동으로 지워줘야함
 
         return container;
     }
 
-    @Bean
-    MessageListenerAdapter adapter(MessageReceiver receiver){
-        return new MessageListenerAdapter(receiver,"receive"); //클래스와 클래스 안의 "메소드명"이 들어간다.
-        // 메세지를 받으면 receiver 객체 안에 있는 receive 메소드를 이용해 처리하겠다.
-    }
+//    @Bean
+//    MessageListenerAdapter adapter(MessageReceiver receiver){ // 어댑터 패턴을 이용
+//        return new MessageListenerAdapter(receiver,"receive"); //클래스와 클래스 안의 "메소드명"이 들어간다.
+//        // 메세지를 받으면 receiver 객체 안에 있는 receive 메소드를 이용해 처리하겠다.
+//    }
 
     @Bean
     MessageReceiver receiver(){
--- mqdemo/src/main/java/kr/ac/inu/mqdemo/receiver/MessageReceiver.java
+++ mqdemo/src/main/java/kr/ac/inu/mqdemo/receiver/MessageReceiver.java
@@ -1,11 +1,15 @@
 package kr.ac.inu.mqdemo.receiver;
 
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
+@RabbitListener(queues = "inu") // 리스너라고 명시해주는 애너테이션(큐는 springConfig에서 입력한 큐의 이름과 동일해야함)
 public class MessageReceiver {
+    @RabbitListener
     public void receive(byte[] message) {          //메세지는 기본적으로 byte로 날아온다.
         System.out.println(new String(message)); //byte형의 메세지를 String형으로 바꿔서 출력한다.
     }
+    @RabbitListener
     public void receive(String message){
             System.out.println(message);
         }
